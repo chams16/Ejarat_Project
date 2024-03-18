aedd3731a8f215f50be8b25fade9648a86f21e79
chams16 <chamseddine.bahri@eniso.u-sousse.tn>
2024-03-18 16:28:03 +0100

    create the cpanel component

---
+++ src/app/component/cPanel/maintenance-request/maintenance-request.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-request',
+  templateUrl: './maintenance-request.component.html',
+  styleUrls: ['./maintenance-request.component.css']
+})
+export class MaintenanceRequestComponent {
+
+}

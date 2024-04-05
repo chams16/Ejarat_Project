a9e16d5116a713339d71e95162b4998cbbf25db5
chams16 <chamseddine.bahri@eniso.u-sousse.tn>
2024-04-05 01:04:16 +0100

    add the ui of add account and search

--- src/app/component/cPanel/chart-account/chart-account.component.ts
+++ src/app/component/cPanel/chart-account/chart-account.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./chart-account.component.css']
 })
 export class ChartAccountComponent {
+  accounts = [
+    { accCode: '1000', accountName: 'CASH', accountType: 'Asset', subAccount: 'Current Asset', balance: 56 },
+    { accCode: '2000', accountName: 'Account Payable', accountType: 'Liability', subAccount: 'Current Liability', balance: 85 },
+    { accCode: '3000', accountName: 'Retained Earning', accountType: 'Owner Equity', subAccount: 'Retained Earnings', balance: 45 },
+    { accCode: '4000', accountName: 'Rent Revenue', accountType: 'Income', subAccount: 'Revenue', balance: 57 },
+    { accCode: '5000', accountName: 'Stationary', accountType: 'Expenses', subAccount: 'General Expenses', balance: 29 }
+];
+  filteredAccounts: any[];
 
+  constructor() {
+      // Initialize filtered accounts with all accounts initially
+      this.filteredAccounts = this.accounts;
+  }
+
+  filterAccounts(event: any) {
+    const searchTerm = event.target.value;
+    // Filter accounts based on account name
+    this.filteredAccounts = this.accounts.filter(account => {
+        return account.accountName.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+}
 }
